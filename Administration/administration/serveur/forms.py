0ee2ef7a4f901517b7b79ea5a6b20289f7ce5d32
quentinhartmann <quentinhartmann13@gmail.com>
2025-06-12 10:10:06 +0200

    Changes by e2401877

--- Administration/administration/serveur/forms.py
+++ Administration/administration/serveur/forms.py
@@ -1,6 +1,7 @@
 from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
 from . import models
+from django import forms
 
 class typeServeurForm(ModelForm):
     class Meta:
@@ -34,6 +35,44 @@ class ServicesForm(ModelForm):
             'ram_ness' : _('RAM nécessaire'),
             'serveur_lanc' : _('Serveur de lancement')
         }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        serveur = cleaned_data.get('serveur_lanc')
+        ram_ness = cleaned_data.get('ram_ness')
+        ram_util = cleaned_data.get('ram_util')
+
+        # 1. Vérifier que RAM utilisée ≤ RAM nécessaire
+        if ram_util is not None and ram_ness is not None:
+            if ram_util > ram_ness:
+                self.add_error('ram_util', _("La RAM utilisée ne peut pas être supérieure à la RAM nécessaire."))
+
+        # 2. Vérifier que la RAM totale demandée ne dépasse pas la RAM du serveur
+        if serveur and ram_ness is not None:
+            existing_services = models.Services.objects.filter(serveur_lanc=serveur)
+            if self.instance.pk:
+                existing_services = existing_services.exclude(pk=self.instance.pk)
+
+            ram_necessaire_actuelle = sum(service.ram_ness for service in existing_services)
+            ram_necessaire_totale = ram_necessaire_actuelle + ram_ness
+
+            if ram_necessaire_totale > serveur.ram:
+                raise forms.ValidationError(
+                    _(
+                        "Ce serveur ne dispose plus de suffisamment de RAM.\n"
+                        "RAM disponible : %(ram_total)s Go\n"
+                        "RAM déjà utilisée : %(ram_utilisee)s Go\n"
+                        "RAM demandée : %(ram_nouvelle)s Go\n"
+                        "RAM totale après ajout : %(ram_totale)s Go"
+                    ) % {
+                        'ram_total': serveur.ram,
+                        'ram_utilisee': ram_necessaire_actuelle,
+                        'ram_nouvelle': ram_ness,
+                        'ram_totale': ram_necessaire_totale
+                    }
+                )
+
+        return cleaned_data
 
 class ApplicationsForm(ModelForm):
     class Meta:
